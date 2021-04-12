a4af97fcc6590e2b76a2fdf81e300f94076de7d8
Christian Sequeira <csequeira153@gmail.com>
2021-04-12 17:11:38 -0300

    fix: table names

--- app/models/barber_shop_model.py
+++ app/models/barber_shop_model.py
@@ -7,7 +7,7 @@ class Barber_shop(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(40), nullable=False, unique=True)
     phone_number = db.Column(db.String(15), nullable=False, unique=True)
-    cnpj = db.Column(db.String(14), nullable=False, unique=False)
+    cnpj = db.Column(db.String(14), nullable=False, unique=True)
     email = db.Column(db.String(120), nullable=False, unique=True)
     password = db.Column(db.String(120), nullable=False, unique=False)
-    address = db.relationship("address", backref="barber_shop")  # Rever
+    address_list = db.relationship("address", backref="barber_shop")
--- app/models/services.py
+++ app/models/services.py
@@ -5,6 +5,6 @@ class Services(db.Model):
     __tablename__ = "services"
 
     id = db.Column(db.Integer, primary_key=True)
-    service_name = db.Column(db.String(40), nullable=False, unique=True)
-    service_price = db.Column(db.String(4), nullable=False, unique=True)
+    service_name = db.Column(db.String(40), nullable=False, unique=False)
+    service_price = db.Column(db.String(4), nullable=False, unique=False)
     barber_id = db.Column(db.Integer, db.ForeignKey("barbers.id"), nullable=False)
