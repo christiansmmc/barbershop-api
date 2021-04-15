d775b232ed877eb54f47482260fb545e1a5ce473
Christian Sequeira <csequeira153@gmail.com>
2021-04-15 18:48:14 -0300

    fix: higher service price limit

--- app/models/services.py
+++ app/models/services.py
@@ -6,7 +6,7 @@ class Services(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     service_name = db.Column(db.String(40), nullable=False, unique=False)
-    service_price = db.Column(db.String(4), nullable=False, unique=False)
+    service_price = db.Column(db.String(20), nullable=False, unique=False)
     barber_id = db.Column(
         db.Integer,
         db.ForeignKey("barbers.id", onupdate="CASCADE", ondelete="CASCADE"),
