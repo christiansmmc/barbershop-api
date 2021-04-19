8c3a38e3e626601f23f417de13d17c332e56142e
Ale <alexandrealvescs@gmail.com>
2021-04-19 15:20:23 -0300

    fix: tratamento de erro KeyError

--- app/views/barber_view.py
+++ app/views/barber_view.py
@@ -12,47 +12,50 @@ bp_barber = Blueprint("bp_barber", __name__, url_prefix="/barber")
 @bp_barber.route("/register/<int:barber_shop_id>", methods=["POST"])
 @jwt_required()
 def register_barber(barber_shop_id):
+    try:
+        current_user = get_jwt()
+        body = request.get_json()
 
-    current_user = get_jwt()
-    body = request.get_json()
+        if (
+            current_user["user_id"] == barber_shop_id
+            and current_user["user_type"] == "barber_shop"
+        ):
 
-    if (
-        current_user["user_id"] == barber_shop_id
-        and current_user["user_type"] == "barber_shop"
-    ):
+            session = current_app.db.session
 
-        session = current_app.db.session
+            name = body["name"]
 
-        name = body.get("name")
+            new_barber = Barbers(
+                name=name, barber_shop_id=barber_shop_id, user_type="barber"
+            )
 
-        new_barber = Barbers(
-            name=name, barber_shop_id=barber_shop_id, user_type="barber"
-        )
+            if "services" in body:
 
-        if "services" in body:
+                for service in body["services"]:
 
-            for service in body["services"]:
+                    new_service = Services(
+                        service_name=service["service_name"],
+                        service_price=service["service_price"],
+                    )
 
-                new_service = Services(
-                    service_name=service["service_name"],
-                    service_price=service["service_price"],
-                )
+                    new_barber.service_list.append(new_service)
 
-                new_barber.service_list.append(new_service)
+            session.add(new_barber)
+            session.commit()
 
-        session.add(new_barber)
-        session.commit()
+            barbershop = Barber_shop.query.filter_by(id=barber_shop_id).first()
 
-        barbershop = Barber_shop.query.filter_by(id=barber_shop_id).first()
+            return {
+                "data": {"barber name": new_barber.name, "barbershop name": barbershop.name}
+            }, HTTPStatus.CREATED
 
-        return {
-            "data": {"barber name": new_barber.name, "barbershop name": barbershop.name}
-        }, HTTPStatus.CREATED
-
-    else:
-        return {
-            "error": "You don't have permission to do this"
-        }, HTTPStatus.UNAUTHORIZED
+        else:
+            return {
+                "error": "You don't have permission to do this"
+            }, HTTPStatus.UNAUTHORIZED
+            
+    except KeyError:
+        return {"msg": "Verify BODY content"}, HTTPStatus.BAD_REQUEST
 
 
 @bp_barber.route("/<int:barber_id>", methods=["DELETE"])
